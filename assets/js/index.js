86d9ab9401b480e6dd85f2de4dcb312f8a16d6e5
Ilmira <tregulovailmira@gmail.com>
2020-08-08 17:42:21 +0300

    Create functions power, maxValue, minValue

---
+++ assets/js/index.js
@@ -0,0 +1,37 @@
+'use strict';
+
+const base = Number(prompt("Enter base number:"));
+const exp = Number(prompt("Enter exp number:"));
+const powerResult = power(base, exp);
+console.log(powerResult);
+
+const number1 = Number(prompt("Enter number1:"));
+const number2 = Number(prompt("Enter number2:"));
+
+const maxNumber = maxValue(number1, number2);
+const minNumber = minValue(number1, number2);
+
+console.log(`Max number: ${maxNumber}`);
+console.log(`Min number: ${minNumber}`);
+
+function power(base, exp) {
+    let result = base;
+    for (let i = 1; i < exp; i++) {
+        result *= base;
+    }
+    return result; 
+}
+
+function maxValue (number1, number2) {
+    if (number1 > number2) {
+        return number1
+    } 
+    return number2;
+}
+
+function minValue (number1, number2) {
+    if (number1 < number2) {
+        return number1
+    } 
+    return number2;
+}
